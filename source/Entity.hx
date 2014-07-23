cf40389587e23d0331ef25fa4ec38968161a49ed
Bernd Ritter <commel@gmail.com>
2014-07-23 23:27:02 +0200

    Gewinnerkennung, Darstellung der Spielstandskarte, Aktionen

--- source/Entity.hx
+++ source/Entity.hx
@@ -2,12 +2,17 @@ package ;
 
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.text.FlxText;
 
 class Entity extends FlxSprite {
-
-  public function new(x:Float=0, y:Float=0) {
+  
+  public function new(x:Float=0, y:Float=0, value:Int=0) {
     super(x,y);
-    makeGraphic(16, 16, FlxColor.BLUE);
+    makeGraphic(32, 32, FlxColor.BLUE);
+
+    if (value > 0) {
+      stamp(new FlxText(x, y, '$value'));
+    }
   }
 
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,13 +7,15 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 
+using flixel.util.FlxSpriteUtil;
+
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
 
-  private var gameField = new Array<Array<Entity>>();
+  private var gameField:Array<Array<Int>> = [ [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0] ];
 
 	/**
 	 * Function that is called up when to state is created to set it up. 
@@ -23,16 +25,69 @@ class PlayState extends FlxState
 		super.create();
     trace("Entered Playstate");
 
-    for (row in 0...3) {
-      for (col in 0...3) {
-        trace('Creating $row/$col');
-        var r = (row+1) * 10;
-        var c = (col+1) * 10;
-        gameField[row][col] = new Entity(r, c);
+    var btn = new FlxButton(100, 100, "Points!", clickForPoints);
+    add(btn);
+    drawState();
+	}
+
+  private function hasWinningCondition():Bool {
+    for (row in 0...4) {
+      for (col in 0...4) {
+        if (getValue(row, col) >= 2048) {
+          return true;
+        }
       }
     }
-	}
-	
+    return false;
+  }
+
+  private function getValue(x:Int, y:Int):Int {
+    return gameField[x][y];
+  }
+
+  private function setValue(x:Int, y:Int, value:Int):Void {
+    gameField[x][y] = value;
+  }
+
+  private function increment(x:Int, y:Int):Void {
+    var previousVal:Int = getValue(x,y);
+
+    setValue(x,y, getNewState(getValue(x,y)));
+  }
+
+  private function getNewState(value:Int):Int {
+    return value == 0 ? 2 : value * 2;
+  }
+
+  private function createEntity(x:Int, y:Int):Entity {
+    return new Entity( (x+1)*30, (y+1)*30, getValue(x,y) );
+  }
+
+  private function drawState():Void {
+    for (row in 0...4) {
+      for (col in 0...4) {
+        add(createEntity(row, col)); 
+      }
+    }
+  }
+
+  private function printState():Void {
+    for (row in gameField) {
+      trace('$row');
+    }
+  }
+
+  private function clickForPoints():Void {
+    increment(0,0);
+    if (hasWinningCondition()) {
+      var winnerText = new FlxText(0, 0, "GEWONNEN!");
+      add(winnerText);
+      winnerText.screenCenter();
+    }
+    printState();
+    drawState();
+  }
+  
 	/**
 	 * Function that is called when this state is destroyed - you might want to 
 	 * consider setting all objects this state uses to null to help garbage collection.
